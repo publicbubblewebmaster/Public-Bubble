4e014f3e19fb6e4fdd0e033ee8965887ad5a1b36
Ian Polding <ipolding@gmail.com>
2015-02-25 00:07:25 +0000

    started db configu wtih sqlite, anorm and events model

--- play-scala/app/models/Event.scala
+++ play-scala/app/models/Event.scala
@@ -1,9 +1,24 @@
 package models
 
+import anorm._
+import play.api.db.DB
+
 /**
  * Created by ian on 24/02/15.
  */
-case class Event (title: String, location: String, description: String)
+case class Event (title: String, location: String, description: String) {
+
+  def getEvent(id : Long) = {
+    DB.withConnection { implicit c =>
+      val result: Boolean = SQL("select * from EVENT where id = {id}").
+        on("id" -> id).execute()
+
+    }
+
+  }
+
+}
+
 
 
 
