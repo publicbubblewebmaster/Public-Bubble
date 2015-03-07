43325e8c14777d6a36bd6d3f0cd9312ade5ae72f
Ian Polding <ipolding@gmail.com>
2015-03-07 01:00:00 +0000

    need to add ajax call and then go server

--- Content-Creation-Stack/js/event-form.js
+++ Content-Creation-Stack/js/event-form.js
@@ -3,17 +3,22 @@ var EventForm = React.createClass(
     {   validate: function() {
             var title = this.refs.title.getDOMNode().value.trim();
             var description = this.refs.description.getDOMNode().value.trim();
+            var address = this.refs.address.getDOMNode().value.trim();
             console.log("title=" + title);
-            console.log("description=" + description);
+            console.log("address=" + description);
+            console.log("description=" + title);
         },
 
 
         handleSubmit : function(e) {
-            console.log("clicked submit");
             e.preventDefault();
-            if (!text || !author) {
-                return;
-            }
+            console.log("clicked submit");
+            this.validate()
+            var title = this.refs.title.getDOMNode().value.trim();
+            var description = this.refs.description.getDOMNode().value.trim();
+            var address = this.refs.address.getDOMNode().value.trim();
+            console.log("this.props.url = " + this.props.url);
+            
             this.props.onCommentSubmit({author: author, text: text});
             this.refs.author.getDOMNode().value = '';
             this.refs.description.getDOMNode().value = '';           
@@ -22,14 +27,13 @@ var EventForm = React.createClass(
         render : function() {
             return (
         <div> 
-        <h1>What does an H1 heading look like?</h1>
+        <h1 className="small-6 small-centered column">Post an Event</h1>
         <form className="small-6 small-centered column" onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="Event Title..." ref="author" className="small-6 small-centered column"/>
+            <input type="text" placeholder="Event Title..." ref="title" className="small-6 small-centered column"/>
             <input type="text" placeholder="Event address..." ref="address" className="small-6 small-centered column"/>
             <textarea placeholder="Event Description..." ref="description" className="small-6 small-centered column"/>
             <input type="submit" value="Post" className="small-6 small-centered column"/>
         </form>
-        <button id="previewButton">Preview</button>
         </div>
            );
         }
@@ -37,6 +41,6 @@ var EventForm = React.createClass(
     );
 
 React.render(
-    <EventForm />,
+    <EventForm url="http://localhost:3000" />,
     document.getElementById('eventForm')
     );
