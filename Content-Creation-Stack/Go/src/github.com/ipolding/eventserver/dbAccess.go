a08d46bd2e9e24dfbcec07cd4b54d863abbead1b
Ian Polding <ipolding@gmail.com>
2015-03-15 22:22:47 +0000

    docker builds and runs locally

--- Content-Creation-Stack/Go/src/github.com/ipolding/eventserver/dbAccess.go
+++ Content-Creation-Stack/Go/src/github.com/ipolding/eventserver/dbAccess.go
@@ -3,32 +3,33 @@ package eventserver
 import (
     "log"
     "database/sql"
-    "github.com/mattn/go-sqlite3"
-)
+    _ "github.com/lib/pq"
+    )
 
 var dataSourceName string
 var DB_DRIVER string
 
 func DbInit(dbLocation string) {
     dataSourceName = dbLocation
-    sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
+    
     }
 
 func PersistEvent(event Event) {
 
-        db, err := sql.Open(DB_DRIVER, dataSourceName)
+        db, err := sql.Open("postgres", dataSourceName)
         log.Print(dataSourceName)
         tx, err := db.Begin()
         if err != nil {
             log.Fatal(err)
         }
          
-        _, err = db.Exec(
-          "INSERT INTO events (title, location, description) values (?, ?, ?)", 
-              event.Title,
-              event.Location, 
-              event.Description,
-              )
+         insert_event, err := db.Prepare("INSERT INTO events(id, title, location, description) values ($1, $2, $3, $4)")
+         if err != nil {
+              log.SetFlags(log.Llongfile)
+              log.Fatal(err)
+         }        
+
+        _, err = insert_event.Exec(2, event.Title, event.Location, event.Description,)
 
         if err != nil {
               log.SetFlags(log.Llongfile)
