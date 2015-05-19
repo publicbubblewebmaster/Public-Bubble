083a5bf3316ba82562ea61251e7ac6fd59e18f27
Ian Polding <ipolding@gmail.com>
2015-05-20 00:16:49 +0100

    added single field form

--- play-scala/app/controllers/Security.scala
+++ play-scala/app/controllers/Security.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import akka.actor.FSM.->
 import com.cloudinary._
 import com.cloudinary.utils.ObjectUtils
 import models.Event
@@ -8,11 +9,18 @@ import play.api.data.{Form, Forms}
 import play.api.libs.json._
 import play.api.mvc._
 
+
+case class UserData(id : String)
+
 object Security extends Controller {
+
+  val userFormTuple = Form(
+    Forms.single("id" -> Forms.text) // tuples come with built-in apply/unapply
+  )
 
   def showLoginForm = Action {
     Ok(views.html.loginForm())
   }
 
-  def handleLogin
 }
+
