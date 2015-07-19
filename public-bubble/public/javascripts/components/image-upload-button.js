34cb5ebbfbfb1d4590bf0b37224a8df375a251b4
Ian Polding <ipolding@gmail.com>
2015-07-19 13:30:34 +0100

    alert box is wrokung, just need to sort layout

--- public-bubble/public/javascripts/components/image-upload-button.js
+++ public-bubble/public/javascripts/components/image-upload-button.js
@@ -1,22 +1,29 @@
 var ImageUploadButton = React.createClass({
     getInitialState : function() {
-            return {
-                    imageAttached:false,
-                    imageUploads : false}},
-
+            return {imageAttached:false,
+                    imageUploads : false,
+                    uploadStatus : 0}
+                    },
     imageAttached : function(event) {
                         this.setState({imageAttached : true})
                     },
-    uploadStatus : function() {
+    uploadStatusChange : function() {
+                        if (this.state.uploadStatus === 500) {
 
-                        return <<div data-alert="" class="alert-box alert round">
+                        return <div data-alert="" className="alert-box alert round">
                                 Upload failed :(
                                </div>;
+                               } else if (this.state.uploadStatus === 0) {
+                        return <div data-alert="" className="alert-box info radius">
+                                                        Please select an image.
+                                                       </div>;
+
+
+                               }
                     },
 
     uploadImage : function() {
 
-                        console.log("image upload button clicked");
                         var x = new FormData()
                         x.append("image1", document.getElementById("image1").files[0]);
                         x.append("domainObject", "blog");
@@ -26,24 +33,25 @@ var ImageUploadButton = React.createClass({
                         x.append("id", blogId)
                         var request = new XMLHttpRequest();
                         request.open("POST", this.props.imageUploadUrl, method="post" );
+                        var uploadStatusCode = 0
 
-                       request.onreadystatechange=function()
+                        var componentReference = this
+                        request.onreadystatechange=function()
                        {
-                         if(request.readyState==4)
-                           this.uploadStatus()
+                         if(request.readyState==4) {
+                             componentReference.setState({uploadStatus: request.status});
+                         }
                        }
-
-                        request.send(x);
-                        console.log("response status = " + request.status);
+                       request.send(x);
                     },
 
     render: function() {
         return (
         <div className="row">
+            {this.uploadStatusChange()}
             <input id="image1" type="file" name="image" onChange={this.imageAttached}></input>
             <button onClick={this.uploadImage} disabled={!this.state.imageAttached}>
             Upload image
-            {this.uploadSuccess()}
             </button>
         </div>
         );
