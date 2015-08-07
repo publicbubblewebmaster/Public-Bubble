1978fd8ae05057eb7980014bf544122a9395b3f4
Ian Polding <ipolding@gmail.com>
2015-08-07 23:22:10 +0100

    Google maps react POC

--- public-bubble/app/controllers/Application.scala
+++ public-bubble/app/controllers/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def aboutUs = Action {
-    Ok(views.html.googleMapTag())
+    Ok(views.html.autocompleteTag())
   }
 
   def twitter = Action {
--- public-bubble/public/javascripts/components/google-location-autocomplete.js
+++ public-bubble/public/javascripts/components/google-location-autocomplete.js
@@ -2,14 +2,13 @@
 
 var GooglePlaceAutocomplete = React.createClass({
 
-    googleInit :  function() {
-        new google.maps.places.Autocomplete(
-        React.findDOMNode(this.refs.placeAutocomplete), null);
+    componentDidMount :  function() {
+        new google.maps.places.Autocomplete(React.findDOMNode(this.refs.placeAutocomplete));
     },
 
     render: function() {
-            return <textarea ref="placeAutocomplete">
-                {this.googleInit()}
-            </textarea>
+            return <input type="text" ref="placeAutocomplete">
+
+            </input>
             }
 });
