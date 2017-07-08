6c9f67e3da4c606301b59e7a501d3ed2646ee365
Ian Polding <ipolding@gmail.com>
2017-07-09 00:12:01 +0100

    Add dropzone dependency

---
+++ public/javascripts/dropzone/dropzone.js
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<html class="" lang="en">
+<head prefix="og: http://ogp.me/ns#">
+<meta charset="utf-8">
+<meta content="IE=edge" http-equiv="X-UA-Compatible">
+<meta content="object" property="og:type">
+<meta content="GitLab" property="og:site_name">
+<meta content="dist/dropzone.js · Artifacts · release (#19127124) · Jobs · Matias Meno / Dropzone" property="og:title">
+<meta content="Dropzone is an easy to use drag&#39;n&#39;drop library. It supports image previews and shows nice progress bars. http://www.dropzonejs.com" property="og:description">
+<meta content="/uploads/system/project/avatar/2712276/dropzone-logo.png" property="og:image">
+<meta content="https://gitlab.com/meno/dropzone/-/jobs/19127124/artifacts/file/dist/dropzone.js" property="og:url">
+<meta content="summary" property="twitter:card">
+<meta content="dist/dropzone.js · Artifacts · release (#19127124) · Jobs · Matias Meno / Dropzone" property="twitter:title">
+<meta content="Dropzone is an easy to use drag&#39;n&#39;drop library. It supports image previews and shows nice progress bars. http://www.dropzonejs.com" property="twitter:description">
+<meta content="/uploads/system/project/avatar/2712276/dropzone-logo.png" property="twitter:image">
+
+<title>dist/dropzone.js · Artifacts · release (#19127124) · Jobs · Matias Meno / Dropzone · GitLab</title>
+<meta content="Dropzone is an easy to use drag&#39;n&#39;drop library. It supports image previews and shows nice progress bars. http://www.dropzonejs.com" name="description">
+<link rel="shortcut icon" type="image/x-icon" href="/assets/favicon-075eba76312e8421991a0c1f89a89ee81678bcde72319dd3e8047e2a47cd3a42.ico" id="favicon" />
+<link rel="stylesheet" media="all" href="/assets/application-bf562c36db4b06702ba455fb509cdac15b6f7b5797112e8a3784394849c9bb3b.css" />
+<link rel="stylesheet" media="print" href="/assets/print-87b4ace0db1f79d91e4fe6e74435b66b71d70fee57ffbb72d0fade17374fcc6b.css" />
+
+
+<script>
+//<![CDATA[
+window.gon={};gon.api_version="v4";gon.default_avatar_url="https:\/\/gitlab.com\/assets\/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png";gon.max_file_size=10;gon.asset_host=null;gon.relative_url_root="";gon.shortcuts_path="\/help\/shortcuts";gon.user_color_scheme="white";gon.katex_css_url="\/assets\/katex-e46cafe9c3fa73920a7c2c063ee8bb0613e0cf85fd96a3aea25f8419c4bfcfba.css";gon.katex_js_url="\/assets\/katex-04bcf56379fcda0ee7c7a63f71d0fc15ffd2e014d017cd9d51fd6554dfccf40a.js";gon.sentry_dsn="https:\/\/526a2f38a53d44e3a8e69bfa001d1e8b@sentry.gitlap.com\/15";gon.gitlab_url="https:\/\/gitlab.com";gon.revision="44d109d";gon.gitlab_logo="\/assets\/gitlab_logo-7ae504fe4f68fdebb3c2034e36621930cd36ea87924c11ff65dbcb8ed50dca58.png";
+//]]>
+</script>
+<script src="/assets/webpack/runtime.1e4f1a3da7dfa5700156.bundle.js"></script>
+<script src="/assets/webpack/common.a208e8b828f4e7f4759b.bundle.js"></script>
+<script src="/assets/webpack/locale.3db4c8eef74bbad2de0a.bundle.js"></script>
+<script src="/assets/webpack/main.7ca17a24789acfd5a781.bundle.js"></script>
+<script src="/assets/webpack/raven.61375d9168165e89d174.bundle.js"></script>
+
+
+
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="qwsUCnsX2cGrQFcylD9wWgrRJQDqlwgIL5LZ9gmi0R9f4I/gXkv9feB0oIp89M++bZUwxNjEW2H7p7pLg+Rn4Q==" />
+<meta content="origin-when-cross-origin" name="referrer">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+<meta content="#474D57" name="theme-color">
+<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-5a9cee0e8a51212e70b90c87c12f382c428870c0ff67d1eb034d884b78d2dae7.png" />
+<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-a6eec6aeb9da138e507593b464fdac213047e49d3093fc30e90d9a995df83ba3.png" sizes="76x76" />
+<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-retina-72e2aadf86513a56e050e7f0f2355deaa19cc17ed97bbe5147847f2748e5a3e3.png" sizes="120x120" />
+<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-retina-8ebe416f5313483d9c1bc772b5bbe03ecad52a54eba443e5215a22caed2a16a2.png" sizes="152x152" />
+<link color="rgb(226, 67, 41)" href="/assets/logo-d36b5212042cebc89b96df4bf6ac24e43db316143e89926c0db839ff694d2de4.svg" rel="mask-icon">
+<meta content="/assets/msapplication-tile-1196ec67452f618d39cdd85e2e3a542f76574c071051ae7effbfde01710eb17d.png" name="msapplication-TileImage">
+<meta content="#30353E" name="msapplication-TileColor">
+
+
+<!-- Piwik -->
+<script>
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.gitlab.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.gitlab.com/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
+
+</head>
+
+<body class="" data-group="" data-page="projects:artifacts:file" data-project="dropzone">
+
+
+<header class="navbar navbar-gitlab with-horizontal-nav">
+<div class="navbar-border"></div>
+<a class="sr-only gl-accessibility" href="#content-body" tabindex="1">Skip to content</a>
+<div class="container-fluid">
+<div class="header-content">
+<div class="dropdown global-dropdown">
+<button class="global-dropdown-toggle" data-toggle="dropdown" type="button">
+<span class="sr-only">Toggle navigation</span>
+<i aria-hidden="true" data-hidden="true" class="fa fa-bars"></i>
+</button>
+<div class="dropdown-menu-nav global-dropdown-menu">
+<ul>
+<li class="home"><a title="Projects" class="dashboard-shortcuts-projects" href="/explore"><div class="shortcut-mappings">
+<div class="key">
+<i aria-label="hidden" class="fa fa-arrow-up"></i>
+P
+</div>
+</div>
+<span>
+Projects
+</span>
+</a></li><li class=""><a title="Groups" class="dashboard-shortcuts-groups" href="/explore/groups"><div class="shortcut-mappings">
+<div class="key">
+<i aria-label="hidden" class="fa fa-arrow-up"></i>
+G
+</div>
+</div>
+<span>
+Groups
+</span>
+</a></li><li class=""><a title="Snippets" class="dashboard-shortcuts-snippets" href="/explore/snippets"><div class="shortcut-mappings">
+<div class="key">
+<i aria-label="hidden" class="fa fa-arrow-up"></i>
+S
+</div>
+</div>
+<span>
+Snippets
+</span>
+</a></li><li class="divider"></li>
+<li class=""><a title="Help" href="/help"><span>
+Help
+</span>
+</a></li></ul>
+
+</div>
+</div>
+<div class="header-logo">
+<a class="home" title="Dashboard" id="logo" href="/"><svg width="28" height="28" class="tanuki-logo" viewBox="0 0 36 36">
+  <path class="tanuki-shape tanuki-left-ear" fill="#e24329" d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"/>
+  <path class="tanuki-shape tanuki-right-ear" fill="#e24329" d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"/>
+  <path class="tanuki-shape tanuki-nose" fill="#e24329" d="M18,34.38 3,14 33,14 Z"/>
+  <path class="tanuki-shape tanuki-left-eye" fill="#fc6d26" d="M18,34.38 11.38,14 2,14 6,25Z"/>
+  <path class="tanuki-shape tanuki-right-eye" fill="#fc6d26" d="M18,34.38 24.62,14 34,14 30,25Z"/>
+  <path class="tanuki-shape tanuki-left-cheek" fill="#fca326" d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"/>
+  <path class="tanuki-shape tanuki-right-cheek" fill="#fca326" d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"/>
+</svg>
+
+</a></div>
+<div class="title-container">
+<h1 class="title"><a href="/meno">Matias Meno</a> / <a class="project-item-select-holder" href="/meno/dropzone">Dropzone</a></h1>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav">
+<li class="hidden-sm hidden-xs">
+<div class="has-location-badge search search-form">
+<form class="navbar-form" action="/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" /><div class="search-input-container">
+<div class="location-badge">This project</div>
+<div class="search-input-wrap">
+<div class="dropdown" data-url="/search/autocomplete">
+<input type="search" name="search" id="search" placeholder="Search" class="search-input dropdown-menu-toggle no-outline js-search-dashboard-options" spellcheck="false" tabindex="1" autocomplete="off" data-toggle="dropdown" data-issues-path="https://gitlab.com/dashboard/issues" data-mr-path="https://gitlab.com/dashboard/merge_requests" aria-label="Search" />
+<div class="dropdown-menu dropdown-select">
+<div class="dropdown-content"><ul>
+<li>
+<a class="is-focused dropdown-menu-empty-link">
+Loading...
+</a>
+</li>
+</ul>
+</div><div class="dropdown-loading"><i aria-hidden="true" data-hidden="true" class="fa fa-spinner fa-spin"></i></div>
+</div>
+<i class="search-icon"></i>
+<i class="clear-icon js-clear-input"></i>
+</div>
+</div>
+</div>
+<input type="hidden" name="group_id" id="group_id" class="js-search-group-options" />
+<input type="hidden" name="project_id" id="search_project_id" value="2712276" class="js-search-project-options" data-project-path="dropzone" data-name="Dropzone" data-issues-path="/meno/dropzone/issues" data-mr-path="/meno/dropzone/merge_requests" />
+<input type="hidden" name="search_code" id="search_code" value="true" />
+<input type="hidden" name="repository_ref" id="repository_ref" />
+
+<div class="search-autocomplete-opts hide" data-autocomplete-path="/search/autocomplete" data-autocomplete-project-id="2712276"></div>
+</form></div>
+
+</li>
+<li class="visible-sm-inline-block visible-xs-inline-block">
+<a title="Search" aria-label="Search" data-toggle="tooltip" data-placement="bottom" data-container="body" href="/search"><i aria-hidden="true" data-hidden="true" class="fa fa-search"></i>
+</a></li>
+<li>
+<div>
+<a class="btn btn-sign-in btn-success" href="/users/sign_in?redirect_to_referer=yes">Sign in</a>
+</div>
+</li>
+</ul>
+</div>
+<button class="navbar-toggle" type="button">
+<span class="sr-only">Toggle navigation</span>
+<i aria-hidden="true" data-hidden="true" class="fa fa-ellipsis-v"></i>
+</button>
+<div class="js-dropdown-menu-projects">
+<div class="dropdown-menu dropdown-select dropdown-menu-projects">
+<div class="dropdown-title"><span>Go to a project</span><button class="dropdown-title-button dropdown-menu-close" aria-label="Close" type="button"><i aria-hidden="true" data-hidden="true" class="fa fa-times dropdown-menu-close-icon"></i></button></div>
+<div class="dropdown-input"><input type="search" id="" class="dropdown-input-field" placeholder="Search your projects" autocomplete="off" /><i aria-hidden="true" data-hidden="true" class="fa fa-search dropdown-input-search"></i><i role="button" aria-hidden="true" data-hidden="true" class="fa fa-times dropdown-input-clear js-dropdown-input-clear"></i></div>
+<div class="dropdown-content"></div>
+<div class="dropdown-loading"><i aria-hidden="true" data-hidden="true" class="fa fa-spinner fa-spin"></i></div>
+</div>
+</div>
+
+</div>
+</div>
+</header>
+
+<script>
+  var findFileURL = "/meno/dropzone/find_file/master";
+</script>
+
+<div class="page-with-sidebar">
+<div class="layout-nav">
+<div class="container-fluid">
+<div class="scrolling-tabs-container">
+<div class="fade-left">
+<i aria-hidden="true" data-hidden="true" class="fa fa-angle-left"></i>
+</div>
+<div class="fade-right">
+<i aria-hidden="true" data-hidden="true" class="fa fa-angle-right"></i>
+</div>
+<ul class="nav-links scrolling-tabs">
+<li class="home"><a title="Project" class="shortcuts-project" href="/meno/dropzone"><span>
+Project
+</span>
+</a></li><li class=""><a title="Repository" class="shortcuts-tree" href="/meno/dropzone/tree/master"><span>
+Repository
+</span>
+</a></li><li class=""><a title="Container Registry" class="shortcuts-container-registry" href="/meno/dropzone/container_registry"><span>
+Registry
+</span>
+</a></li><li class=""><a title="Issues" class="shortcuts-issues" href="/meno/dropzone/issues"><span>
+Issues
+<span class="badge count issue_counter">3</span>
+</span>
+</a></li><li class=""><a title="Merge Requests" class="shortcuts-merge_requests" href="/meno/dropzone/merge_requests"><span>
+Merge Requests
+<span class="badge count merge_counter js-merge-counter">5</span>
+</span>
+</a></li><li class="active"><a title="Pipelines" class="shortcuts-pipelines" href="/meno/dropzone/pipelines"><span>
+Pipelines
+</span>
+</a></li><li class=""><a title="Wiki" class="shortcuts-wiki" href="/meno/dropzone/wikis/home"><span>
+Wiki
+</span>
+</a></li><li class=""><a title="Settings" class="shortcuts-tree" href="/meno/dropzone/settings/members"><span>
+Settings
+</span>
+</a></li><li class="hidden">
+<a title="Activity" class="shortcuts-project-activity" href="/meno/dropzone/activity"><span>
+Activity
+</span>
+</a></li>
+<li class="hidden">
+<a title="Network" class="shortcuts-network" href="/meno/dropzone/network/master">Graph
+</a></li>
+<li class="hidden">
+<a title="Charts" class="shortcuts-repository-charts" href="/meno/dropzone/graphs/master/charts">Charts
+</a></li>
+<li class="hidden">
+<a class="shortcuts-new-issue" href="/meno/dropzone/issues/new">Create a new issue
+</a></li>
+<li class="hidden">
+<a title="Jobs" class="shortcuts-builds" href="/meno/dropzone/-/jobs">Jobs
+</a></li>
+<li class="hidden">
+<a title="Commits" class="shortcuts-commits" href="/meno/dropzone/commits/master">Commits
+</a></li>
+<li class="hidden">
+<a title="Issue Boards" class="shortcuts-issue-boards" href="/meno/dropzone/boards">Issue Boards</a>
+</li>
+</ul>
+</div>
+
+</div>
+</div>
+<div class="scrolling-tabs-container sub-nav-scroll">
+<div class="fade-left">
+<i aria-hidden="true" data-hidden="true" class="fa fa-angle-left"></i>
+</div>
+<div class="fade-right">
+<i aria-hidden="true" data-hidden="true" class="fa fa-angle-right"></i>
+</div>
+
+<div class="nav-links sub-nav scrolling-tabs">
+<ul class="container-fluid container-limited">
+<li class=""><a title="Pipelines" class="shortcuts-pipelines" href="/meno/dropzone/pipelines"><span>
+Pipelines
+</span>
+</a></li><li class="active"><a title="Jobs" class="shortcuts-builds" href="/meno/dropzone/-/jobs"><span>
+Jobs
+</span>
+</a></li><li class=""><a title="Schedules" class="shortcuts-builds" href="/meno/dropzone/pipeline_schedules"><span>
+Schedules
+</span>
+</a></li><li class=""><a title="Charts" class="shortcuts-pipelines-charts" href="/meno/dropzone/pipelines/charts"><span>
+Charts
+</span>
+</a></li></ul>
+</div>
+</div>
+
+<div class="content-wrapper page-with-layout-nav page-with-sub-nav">
+<div class="alert-wrapper">
+
+
+<div class="flash-container flash-container-page">
+</div>
+
+
+</div>
+<div class="container-fluid container-limited ">
+<div class="content" id="content-body">
+
+<div class="content-block build-header top-area">
+<div class="header-content">
+<span class="ci-status ci-success " title="">
+<svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg"><g fill-rule="evenodd"><path d="M0 7a7 7 0 1 1 14 0A7 7 0 0 1 0 7z"/><path d="M13 7A6 6 0 1 0 1 7a6 6 0 0 0 12 0z" fill="#FFF"/><path d="M6.278 7.697L5.045 6.464a.296.296 0 0 0-.42-.002l-.613.614a.298.298 0 0 0 .002.42l1.91 1.909a.5.5 0 0 0 .703.005l.265-.265L9.997 6.04a.291.291 0 0 0-.009-.408l-.614-.614a.29.29 0 0 0-.408-.009L6.278 7.697z"/></g></svg>
+
+passed
+</span>
+
+<strong>
+Job
+<a class="js-build-id" href="/meno/dropzone/-/jobs/19127124">#19127124</a>
+</strong>
+in pipeline
+<strong>
+<a href="/meno/dropzone/pipelines/9164341">#9164341</a>
+</strong>
+for
+<strong>
+<a class="commit-sha" href="/meno/dropzone/commit/e50c797a1ba4a9142720db34a27e22af325cf0dc">e50c797a</a>
+</strong>
+from
+<strong>
+<a class="ref-name" href="/meno/dropzone/commits/master">master</a>
+</strong>
+by
+<a href="/meno">
+<span class="hidden-xs">
+<img class="avatar s24" src="https://secure.gravatar.com/avatar/ce44e57e4bc9b51f9692e17bae6319e7?s=48&amp;d=identicon" alt="Ce44e57e4bc9b51f9692e17bae6319e7?s=48&amp;d=identicon" />
+<strong data-placement="top" data-title="@meno" data-toggle="tooltip">
+Matias Meno
+</strong>
+</span>
+<strong class="visible-xs-inline">@meno</strong>
+</a>
+
+<time class="js-timeago" title="Jun 20, 2017 1:37pm" datetime="2017-06-20T13:37:27Z" data-toggle="tooltip" data-placement="top" data-container="body">Jun 20, 2017</time>
+</div>
+</div>
+
+<div class="tree-holder" id="tree-holder">
+<div class="nav-block">
+<ul class="breadcrumb repo-breadcrumb">
+<li>
+<a href="/meno/dropzone/-/jobs/19127124/artifacts/browse">Artifacts</a>
+</li>
+<li>
+<a href="/meno/dropzone/-/jobs/19127124/artifacts/browse/dist">dist</a>
+</li>
+<li>
+<a href="/meno/dropzone/-/jobs/19127124/artifacts/file/dist/dropzone.js"><strong>dropzone.js</strong>
+</a></li>
+</ul>
+</div>
+<article class="file-holder">
+<div class="js-file-title file-title-flex-parent">
+<div class="file-header-content">
+<i aria-hidden="true" data-hidden="true" class="fa fa-file-text-o fa-fw"></i>
+<strong class="file-title-name">
+dropzone.js
+</strong>
+<button class="btn btn-clipboard btn-transparent prepend-left-5" data-toggle="tooltip" data-placement="bottom" data-container="body" data-class="btn-clipboard btn-transparent prepend-left-5" data-title="Copy file path to clipboard" data-clipboard-text="{&quot;text&quot;:&quot;dist/dropzone.js&quot;,&quot;gfm&quot;:&quot;`dist/dropzone.js`&quot;}" type="button" title="Copy file path to clipboard" aria-label="Copy file path to clipboard"><i aria-hidden="true" aria-hidden="true" data-hidden="true" class="fa fa-clipboard"></i></button>
+<small>
+71.8 KB
+</small>
+</div>
+
+<div class="file-actions hidden-xs">
+
+<div class="btn-group" role="group"><a class="btn btn-sm has-tooltip" target="_blank" rel="noopener noreferrer" title="Download" data-container="body" href="/meno/dropzone/-/jobs/19127124/artifacts/raw/dist/dropzone.js"><i aria-hidden="true" data-hidden="true" class="fa fa-download"></i></a></div>
+</div>
+</div>
+<div class="blob-viewer" data-type="simple">
+<div class="file-content code">
+<div class="nothing-here-block">
+The source could not be displayed because it is stored as a job artifact.
+You can
+<a target="_blank" rel="noopener noreferrer" href="/meno/dropzone/-/jobs/19127124/artifacts/raw/dist/dropzone.js">download it</a>
+instead.
+</div>
+</div>
+
+</div>
+
+
+</article>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
+
