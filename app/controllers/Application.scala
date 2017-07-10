ccf3373c9795c7268bfd27dabed8152bb6cf34ab
Ian Polding <ipolding@gmail.com>
2017-07-10 23:58:06 +0100

    Fix compile

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,29 @@
 package controllers
-
+import models.Member
 import play.api.mvc._
-import dao.CommitteeDao
+import dao.SlickCommiteeDao
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
 
 object Application extends Controller {
 
-  def aboutUs = Action {
-    val dao = new CommitteeDao()
-    Ok(views.html.aboutUs(dao.listMembers()))
+  lazy val HOME_404 = NotFound(views.html.noContent("Home"))
+
+
+  lazy val committeDao = new SlickCommiteeDao()
+
+  def aboutUs = Action.async { implicit request =>
+
+    committeDao.listMembers.map {commitee =>
+      Ok(views.html.aboutUs(commitee))
+
+    }.fallbackTo(Future{HOME_404})
   }
+
+
 
   def twitter = Action {
     Ok(views.html.twitter("Twitter"))
---
+++ app/dao/SlickCommiteeDao.scala
@@ -0,0 +1,64 @@
+package dao
+
+import play.api.{Logger}
+import java.sql.Date
+import models.Member
+import play.api.Play
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfig}
+import slick.backend.DatabaseConfig
+import slick.driver.PostgresDriver.api._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import slick.driver.JdbcProfile
+import scala.concurrent.Future
+
+trait CommiteeComponent {
+  self: HasDatabaseConfig[JdbcProfile] =>
+
+  class Commitee(tag: Tag) extends Table[Member](tag, Some("public_bubble"), "commitee") {
+
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def description = column[String]("description")
+
+    def imageUrl = column[String]("image_url")
+
+    // the ? method lifts the column into an option
+    def * = (id.?, description, imageUrl) <> ((Member.apply _).tupled, Member.unapply)
+
+    // the default projection is Member
+
+  }
+
+}
+
+class SlickCommiteeDao extends HasDatabaseConfig[JdbcProfile] with CommiteeComponent {
+
+  private val commitee = TableQuery[Commitee]
+
+  override protected val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+
+//  override def update(Member: Member): Unit = {
+//    val query = for {b <- commitee if b.id === Member.id} yield
+//      (b.title, b.intro, b.author, b.content, b.publishDate)
+//
+//    db.run(query.update(Member.title, Member.intro, Member.author, Member.content, Member.publishDate))
+//  }
+
+//  override def addImage(id: Long, url: String): Future[Boolean] = {
+//    val q = for {b <- commitee if b.id === id} yield b.image1Url
+//    val updateImage = q.update(Some(url))
+//    db.run(updateImage).map(_ == 1)
+//  }
+
+  def delete(id: Long): Future[Int] = {
+    val findById = commitee.filter(_.id === id)
+    dbConfig.db.run(findById.delete)
+  }
+
+  def create(Member: Member) = {
+    dbConfig.db.run(commitee += Member)
+  }
+
+  def listMembers: Future[Seq[Member]] = db.run(commitee.result)
+
+}
